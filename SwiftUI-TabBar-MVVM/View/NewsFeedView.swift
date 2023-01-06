7a33e15e40c5c003612b9ffb0bdf0a0d5b079e5b
Shashank Mishra <shashankm@exzeo.com>
2023-01-06 15:04:35 +0530

    Add network monitor

--- SwiftUI-TabBar-MVVM/View/NewsFeedView.swift
+++ SwiftUI-TabBar-MVVM/View/NewsFeedView.swift
@@ -10,28 +10,34 @@ import SwiftUI
 struct NewsFeedView: View {
     
     @ObservedObject private var viewModel = NewsFeedViewModel()
+    @ObservedObject var monitor = NetworkMonitor()
     
     @Environment(\.managedObjectContext) private var viewContext
     
     @FetchRequest(entity: NewsFeed.entity(), sortDescriptors: [])
     
     private var favFeeds: FetchedResults<NewsFeed>
-        
+    
     var body: some View {
         NavigationView {
             VStack {
-                List {
-                    ForEach(viewModel.newsFeed?.hits ?? []) { feed in
-                        HStack {
-                            Text(feed.title)
-                            Spacer()
-                            Button {
-                                addFavorite(feed)
-                            } label: {
-                                Image(systemName: "star")
+                if monitor.isConnected {
+                    List {
+                        ForEach(viewModel.newsFeed?.hits ?? []) { feed in
+                            HStack {
+                                Text(feed.title)
+                                Spacer()
+                                Button {
+                                    addFavorite(feed)
+                                } label: {
+                                    Image(systemName: "star")
+                                }
                             }
                         }
                     }
+                }
+                else {
+                    Text("No Intenet Connection")
                 }
             }
             .onAppear(){
@@ -42,29 +48,29 @@ struct NewsFeedView: View {
     }
     
     private func addFavorite(_ feedHit: Hit) {
-           
-           withAnimation {
-               let feed = NewsFeed(context: viewContext)
-               print(feedHit.objectID)
-               feed.objectId = feedHit.objectID
-               feed.id = UUID()
-               feed.title = feedHit.title
-               feed.url = feedHit.url
-               feed.isFavourite = true
-               
-               saveContext()
-           }
-       }
-       
-       private func saveContext() {
-           do {
-               try viewContext.save()
-           } catch {
-               let error = error as NSError
-               fatalError("An error occured: \(error)")
-           }
-       }
-
+        
+        withAnimation {
+            let feed = NewsFeed(context: viewContext)
+            print(feedHit.objectID)
+            feed.objectId = feedHit.objectID
+            feed.id = UUID()
+            feed.title = feedHit.title
+            feed.url = feedHit.url
+            feed.isFavourite = true
+            
+            saveContext()
+        }
+    }
+    
+    private func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            let error = error as NSError
+            fatalError("An error occured: \(error)")
+        }
+    }
+    
 }
 
 struct NewsView_Previews: PreviewProvider {
